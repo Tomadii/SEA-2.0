942f1af81b7742431aa813297e58f1f30919665a
thorchem <Thomas@tomadi.de>
2021-05-19 13:50:49 +0200

    Klasse für Person mit Anrede angelegt

--- src/main/java/de/telekom/sea2/looup/Salutation.java
+++ src/main/java/de/telekom/sea2/looup/Salutation.java
@@ -1,5 +1,42 @@
 package de.telekom.sea2.looup;
 
-public class Salutation {
+public enum Salutation {
 
+	FRAU,
+	HERR,
+	DIVERS;
+	
+	public static Salutation fromString(String string) {
+		switch (string.toUpperCase()) {
+			case "FRAU":
+			case "F":
+				return FRAU;
+			case "HERR":
+			case "H":
+			case "MANN":
+			case "M":
+				return HERR;
+			case "DIVERS":
+			case "D":
+				return DIVERS;
+			default:
+				throw new IllegalArgumentException("Unerwarteter Wert: " + string);
+		
+		}
+	}
+	
+	@Override
+	public String toString() {
+		switch (this) {
+			case FRAU:
+				return "Frau";
+			case HERR:
+				return "Herr";
+			case DIVERS:
+				return "Divers";
+			default:
+				throw new IllegalArgumentException("Unerwarteter Fall");
+		}
+	}
+	
 }
--- src/main/java/de/telekom/sea2/model/Person.java
+++ src/main/java/de/telekom/sea2/model/Person.java
@@ -1,5 +1,44 @@
 package de.telekom.sea2.model;
 
+import de.telekom.sea2.looup.Salutation;
+
 public class Person {
 
+	private String firstname;
+	private String lastname;
+	private Salutation salutation;
+
+	public Person() {
+	}
+	
+	public Person(final Salutation salutation, final String firstname, final String lastname) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.salutation = salutation;
+	}
+	
+	public String getFirstname() {
+		return firstname;
+	}
+	
+	public void setFirstname(final String firstname) {
+		this.firstname = firstname;
+	}
+	
+	public String getLastname() {
+		return lastname;
+	}
+	
+	public void setLastname(final String lastname) {
+		this.lastname = lastname;
+	}
+	
+	public Salutation getSalutation() {
+		return salutation;
+	}
+
+	public void setSalutation(Salutation salutation) {
+		this.salutation = salutation;
+	}
+	
 }
